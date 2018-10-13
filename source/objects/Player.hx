3a24460704b80fe200913513571d7400665cb44a
Alexander <MrRandomgamerdkHD@gmail.com>
2018-10-13 18:57:21 +0200

    Simple attack hitbox

--- source/objects/Player.hx
+++ source/objects/Player.hx
@@ -8,17 +8,26 @@ class Player extends FlxSprite
 {
     public var playerWidth = 64;
     public var playerHeight = 64;
+    public var playerAttackBox:FlxSprite;
 
     public function new (x:Float, y:Float)
     {
         super(x, y);
         makeGraphic(playerWidth, playerHeight, FlxColor.BLUE);
-
+        playerAttackBox = new FlxSprite(x - playerWidth, y);
+        playerAttackBox.makeGraphic(playerWidth, playerHeight, FlxColor.RED);
+        Global.PS.add(playerAttackBox);
     }
 
     override public function update(elapsed:Float):Void 
     {
-
         super.update(elapsed);
+        if (FlxG.keys.justPressed.LEFT) 
+        {
+            playerAttackBox.x = this.x - playerWidth;
+        } else if (FlxG.keys.justPressed.RIGHT)
+        {
+            playerAttackBox.x = this.x + playerWidth;
+        }
     }
 }
